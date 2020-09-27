f876acbdefbfa9d18544ad46cf345f935c1024fd
Eduardo Augusto Burger <32910@PROWAY.TREINA>
2020-09-27 16:50:32 -0300

    Utilizando metodos Acessores e qualificadores

--- Zoologico/src/br/com/logica/zoologico/classes/Animal.java
+++ Zoologico/src/br/com/logica/zoologico/classes/Animal.java
@@ -1,12 +1,40 @@
 package br.com.logica.zoologico.classes;
 
+
+
 public class Animal {
 	
 	//Atributos ou Proriedades do Objeto
 	//Neste momento as Propriedades sao Publicas
 	 private String nome;
-	 public String especie;
-	 public int idade;
+	 private String especie;
+	 private int idade;
 	
+public void setNome(String nome) {
+	this.nome = nome;
+	
+}
+public String getNome() {
+	return nome;
+}
 
+	public void  setEspecie(String especie) {
+		this.especie=especie;
+		
+		
+}
+	
+	public String getEspecie() {
+		return especie;
+	}
+	
+	public void setIdade(int idade) {
+		
+		this.idade=idade;
+	}
+	
+	public int getIdade() {
+		return idade;
+	}
+	
 }
