e9a385fe8c2b90f722628e34b8b28cd6ecc7067b
Eduardo Augusto Burger <32910@PROWAY.TREINA>
2020-10-04 17:41:54 -0300

    Mais implementaçao de sobre escrita de metodos

--- Zoologico/src/br/com/logica/zoologico/heranca/Gato.java
+++ Zoologico/src/br/com/logica/zoologico/heranca/Gato.java
@@ -4,4 +4,14 @@ package br.com.logica.zoologico.heranca;
 
 public class Gato extends Animal {
 
+
+@Override
+public  boolean isAdulto() {
+	if(estaVivo) {
+		
+		return idade >=2;
+	}else {
+		return false;
+	}
+	
 }
